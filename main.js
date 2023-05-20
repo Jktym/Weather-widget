a77c34e2f867cdc828a0f09537d440e5d92e380f
Jktym <97807673+Jktym@users.noreply.github.com>
2023-05-20 07:24:10 +0700

    Final

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const ApiKey = 'b8914a77fc7a4227b8c224125231905';
-
-
-//Получаем название города
 const header = document.querySelector('.header')
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
+
+
+
+
+
+function removeOldInfo()
+{
+    //Очистка прошлого результата
+    const prevInfo = document.querySelector('.info');
+    if(prevInfo) prevInfo.remove();
+}
 
 form.onsubmit = function(e)
 {   
@@ -22,8 +30,7 @@ form.onsubmit = function(e)
         return response.json()
     }).then((data) =>
     {
-        const prevInfo = document.querySelector('.info');
-        if(prevInfo) prevInfo.remove();
+        removeOldInfo();
         //Проверка на наличие ошибки
         if(data.error)
         {
@@ -37,14 +44,14 @@ form.onsubmit = function(e)
             console.log(data.current.condition.text);
             console.log(data.current.temp_c);
     
-            //Очистка прошлого результата
+    
 
     
             const html = `<div class="info">
                             <h2 class="info-city">${city}</h2>
                             <div class="info-weather">
                                 <div class="info-temperature">${data.current.temp_c}°C</div>
-                                <img class="info-img" src="./img/cloud/35.png" alt="Погода">
+                                <img class="info-img" src="https:${data.current.condition.icon}" alt="Погода">
                                 </div>
                                 <div class="info-description">${data.current.condition.text}</div>
                         </div>`;
